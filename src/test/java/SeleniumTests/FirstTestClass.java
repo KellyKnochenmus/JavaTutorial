93e48924e771154b81c39754d11a42b00bce636f
KellyKnochenmus <kelly_k@verizon.net>
2017-11-08 09:59:13 -0600

    updating

---
+++ src/test/java/SeleniumTests/FirstTestClass.java
@@ -0,0 +1,8 @@
+package SeleniumTests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstTestClass {
+    WebDriver driver = new FirefoxDriver();
+}
